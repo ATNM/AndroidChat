c990cf96e00d6627b9751cd98ccf73e6cbe4e81a
Alex Serban <aserban@airtouchmedia.com>
2016-07-26 14:52:00 +0200

    Added Message View Holder Class

---
+++ app/src/main/java/com/airtouch/adrian/chatex2/MessageViewHolder.java
@@ -0,0 +1,32 @@
+package com.airtouch.adrian.chatex2;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by atnm1 on 26/07/16.
+ */
+public class MessageViewHolder extends RecyclerView.ViewHolder {
+
+    public LinearLayout bubbleView;
+    public LinearLayout messageContainer;
+    public TextView messageTextView;
+    public TextView nameTextView;
+
+
+    public MessageViewHolder(View v){
+        super(v);
+
+        bubbleView = (LinearLayout) v.findViewById(R.id.bubble);
+        messageContainer = (LinearLayout) v.findViewById(R.id.message_container);
+        messageTextView = (TextView) v.findViewById(R.id.messageTextView);
+        nameTextView = (TextView) v.findViewById(R.id.nameTextView);
+
+    }
+
+
+}
